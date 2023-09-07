027b760d34d7b5ae95ea36444583c093c16d9ea1
Osori <kunshim@naver.com>
2023-09-07 16:56:10 +0900

    코드 정리 및 주석 추가

--- Calltrack.h
+++ Calltrack.h
@@ -7,21 +7,26 @@
 #include <memory>
 #include "Image.h"
 #include "Call.h"
+#include "fstream"
 
 class CallTrack 
 { 
 private:
+    void printInnerCallCount();
     std::tr1::unordered_map<ADDRINT, Call*> callCache;
-    int sequence = 0;
-public:
-    Call* root;
+    size_t sequence = 0;
     size_t hash;
+    Call* root;
     Call* currentCall;
+public:
     CallTrack(ADDRINT addr, ADDRINT sfp);
-    void insertChild(Call* call);
-    void insertBranch(Call* call);
+    Call* getRootCall();
+    Call* getCurrentCall();
+    void insertChild(const Call* call);
+    void insertBranch(const Call* call);
+    void saveCoverage();
+    void saveIndirect();
     void descript();
-    void analyze();
     ~CallTrack();
     bool operator==(const CallTrack& other) const;
 };
